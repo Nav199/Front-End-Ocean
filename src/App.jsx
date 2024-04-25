7905e3c1e915d2e8d2da8b098c5d8ccf4285e42c
Victor <navecavictor@gmail.com>
2024-04-25 19:32:15 -0300

    versão main

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+
+import './App.css'
+import Card from './componentes_Card/Card/Card'
+
+
+
+function App() {
+    const item1 = {
+      nome:'Rick Sanches',
+      image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+      tags: ['Status: Vivo', 'Espécie: Humana', 'Origem: Terra C-137']
+    }
+    const item2 = {
+      nome:'Morty Smith',
+      image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg',
+      tags: ['Status: Vivo']
+    }
+    const item3 = {
+      nome:'Summer Smith',
+      image:'https://rickandmortyapi.com/api/character/avatar/3.jpeg',
+      tags: ['Espécie: Humana']
+    }
+
+    const itens = [item1,item2,item3]
+  return (
+    <>
+    <div className="cards">
+        {itens.map(function (elemento) {
+          return <Card item={elemento} />
+        })}
+      </div>
+    </>
+  )
+}
+
+export default App
